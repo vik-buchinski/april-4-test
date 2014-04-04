199ad291c23dbc1bf7967c0bd53d8f83eeb51fd6
Pavel Zubkou <pavel.zubkou@gmail.com>
2014-04-04 11:03:25 +0300

    Добавить решение контрольной

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,19 @@
+var users = [];
 
 /*
  * GET users listing.
  */
 
-exports.list = function(req, res){
-  res.send("respond with a resource");
+exports.list = function (req, res) {
+  res.json(users);
+};
+
+exports.create = function (req, res) {
+  users.push({ id: users.length + 1, name: req.body.name });
+  res.json(users[users.length - 1]);
+};
+
+exports.destroyList = function (req, res) {
+  users = [];
+  res.json(users);
 };
