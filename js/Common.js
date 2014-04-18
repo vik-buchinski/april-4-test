7d647e2ad3ad0f99aae0b685b30c1f8ebe34131f
Viktor <vik-buchinski@ya.ru>
2014-04-18 09:35:15 +0300

    Нарисована вьюшка и немного логики в .js фале, остальное не успел(

---
+++ js/Common.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    console.log("ready!");
+});
+
+var homePage = {
+    buildView: function () {
+        var students = new Array(
+            new Student("first", null, null),
+            new Student("second", null, null),
+            new Student("third", null, null));
+
+        var data = {
+            students: students
+        };
+        
+        $.mobile.changePage(
+    }
+};
+
+function initiateData() {
+}
+
+function saveStudents(students) {
+    localStorage.setItem("students", students);
+}
---
+++ model/models.js
@@ -0,0 +1,5 @@
+﻿function Student(name, disciplinsList, marksList) {
+    this.name = name;
+    this.disciplins_list = disciplinsList;
+    this.marks_list = marksList;
+}
